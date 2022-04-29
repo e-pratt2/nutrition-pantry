9d01fb1932b207121a36f4bf6cd561720a7edb18
Anna-Mclenko <amikhailenko@ewu.edu>
2022-04-29 11:13:36 -0700

    get Calories in Nutrition

--- src/Nutrition.java
+++ src/Nutrition.java
@@ -3,6 +3,10 @@ import java.util.Objects;
 public class Nutrition {
     private double calories, fat, sugar, fiber, protein, sodium;
 
+    public double getCalories(double calories){
+        this.calories = calories;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(calories, fat, sugar, fiber, protein, sodium);
