2a3b351c55f7cec31992856420d97b0b1063ada1
e-pratt2 <epratt2@ewu.edu>
2022-04-29 11:12:44 -0700

    Nutrition constructor

--- src/Nutrition.java
+++ src/Nutrition.java
@@ -3,6 +3,15 @@ import java.util.Objects;
 public class Nutrition {
     private double calories, fat, sugar, fiber, protein, sodium;
 
+    public Nutrition(double calories, double fat, double sugar, double fiber, double protein, double sodium) {
+        this.calories = calories;
+        this.fat = fat;
+        this.sugar = sugar;
+        this.fiber = fiber;
+        this.protein = protein;
+        this.sodium = sodium;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(calories, fat, sugar, fiber, protein, sodium);
