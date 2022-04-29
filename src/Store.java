c478a00007b81154c4c520a35d76223971aa390d
Anna-Mclenko <amikhailenko@ewu.edu>
2022-04-29 11:29:07 -0700

    Store file added constructor, getPrice and add receipt methods

--- src/Store.java
+++ src/Store.java
@@ -2,6 +2,23 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Store {
+
     private HashMap<Grocery, Double> groceryPrices;
     private ArrayList<Receipt> receipts;
+    private String name;
+
+    public void Store(String name){
+        this.groceryPrices = new HashMap<Grocery, Double>();
+        this.receipts = new ArrayList<Receipt>();
+        this.name = name;
+    }
+
+    public double getPriceOf(Grocery grocery){
+        Double price = groceryPrices.get(grocery);
+        return price == null ? 0.0 : price.doubleValue();
+    }
+
+    public void addR(Receipt receipt){
+        this.receipts.add(receipt);
+    }
 }
