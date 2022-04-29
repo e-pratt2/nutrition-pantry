59507214ad3f1eca364ef47b553bd446caf95394
e-pratt2 <epratt2@ewu.edu>
2022-04-29 11:08:19 -0700

    hashCode for nutrition and grocery

--- src/Grocery.java
+++ src/Grocery.java
@@ -1,5 +1,12 @@
+import java.util.Objects;
+
 public class Grocery {
 
     private String name;
     private Nutrition nutrition;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, nutrition);
+    }
 }
--- src/Nutrition.java
+++ src/Nutrition.java
@@ -1,3 +1,10 @@
+import java.util.Objects;
+
 public class Nutrition {
     private double calories, fat, sugar, fiber, protein, sodium;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(calories, fat, sugar, fiber, protein, sodium);
+    }
 }
