70a90cd0808ebd2e61e507a4a4d8f2b53d202255
e-pratt2 <epratt2@ewu.edu>
2022-04-29 11:13:14 -0700

    Grocery constructor

--- src/Grocery.java
+++ src/Grocery.java
@@ -5,6 +5,11 @@ public class Grocery {
     private String name;
     private Nutrition nutrition;
 
+    public Grocery(String name, Nutrition nutrition) {
+        this.name = name;
+        this.nutrition = nutrition;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(name, nutrition);
