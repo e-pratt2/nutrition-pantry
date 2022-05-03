6184ee8a234d353a7821ed5699272faee87f9f06
e-pratt2 <epratt2@ewu.edu>
2022-05-03 11:10:30 -0700

    implement UPC validity check

--- src/Factories/UPCGroceryFactory.java
+++ src/Factories/UPCGroceryFactory.java
@@ -8,16 +8,47 @@ public class UPCGroceryFactory implements GroceryFactory {
     public Grocery createGrocery() {
         return null;
     }
-    private String promptUPC() {
+    private static String promptUPC() {
         return null;
     }
-    private boolean validateUPC(String upc) {
-        return false;
+
+    /**
+     * @param upc A 12-digit numeric UPC string to checksum for validity
+     * @return whether the UPC represents a valid code according to the UPC-12 standard
+     */
+    public static boolean validateUPC(String upc) {
+        //UPC-12 must be 12 digits
+        if(upc.length() != 12) {
+            return false;
+        }
+
+        //Sum up the digits...
+        int digitSum = 0;
+        for(int i = 0; i < 11; ++i) {
+            //Get the digit at the current index
+            char c = upc.charAt(i);
+            int digit = Character.digit(c, 10);
+            //Not a digit - invalid code
+            if(digit == -1)
+                return false;
+            //Even numbered indices get multiplied by 3
+            if((i & 1) == 0)
+                digit *= 3;
+            //Add to our sum
+            digitSum += digit;
+        }
+
+        //The value of the check digit should be whatever makes the total
+        //sum a multiple of 10
+        int desiredCheckDigit = 10 - digitSum % 10;
+        char checkChar = upc.charAt(11);
+        //Return if the actual check digit matches our theoretical checksum
+        return Character.digit(checkChar, 10) == desiredCheckDigit;
     }
-    private String getOpenFoodFactsJSON(String upc) {
+    private static String getOpenFoodFactsJSON(String upc) {
         return null;
     }
-    private Nutrition parseJSON(String json) {
+    private static Nutrition parseJSON(String json) {
         return null;
     }
 
--- src/NutritionPantry.java
+++ src/NutritionPantry.java
@@ -1,5 +1,7 @@
+import Factories.UPCGroceryFactory;
+
 public class NutritionPantry {
     public static void main(String[] args) {
-
+        System.out.println(UPCGroceryFactory.validateUPC("016000264793"));
     }
 }
