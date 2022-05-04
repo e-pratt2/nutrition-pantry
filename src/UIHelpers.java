e7d024e3ef2fe25c0bd338507748367ea8ba0596
e-pratt2 <epratt2@ewu.edu>
2022-05-04 10:25:38 -0700

    promptDouble

--- src/UIHelpers.java
+++ src/UIHelpers.java
@@ -1,6 +1,16 @@
-public class UIHelpers {
-    public double promptDouble(String prompt) {
+import java.util.Scanner;
 
+public class UIHelpers {
+    public static double promptDouble(String prompt) {
+        Scanner s = new Scanner(System.in);
+        while(true) {
+            try {
+                String line = s.nextLine();
+                return Double.parseDouble(line);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid value, try again.");
+            }
+        }
     }
     public String promptString(String prompt) {
 
