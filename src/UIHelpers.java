98123d195200941a9abb2419be42f37e8c825e13
Anna-Mclenko <amikhailenko@ewu.edu>
2022-05-04 10:25:16 -0700

    String Scanner UIHelpers

--- src/UIHelpers.java
+++ src/UIHelpers.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class UIHelpers {
     public double promptDouble(String prompt) {
 
     }
     public String promptString(String prompt) {
 
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        return str;
     }
 }
