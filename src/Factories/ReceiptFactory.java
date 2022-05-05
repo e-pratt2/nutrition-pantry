155dc8d75367765af94c1990274b66e4b3d21d16
e-pratt2 <epratt2@ewu.edu>
2022-05-05 10:52:07 -0700

    Receipt prompt start

--- src/Factories/ReceiptFactory.java
+++ src/Factories/ReceiptFactory.java
@@ -1,9 +1,21 @@
 package Factories;
 
 import Database.Receipt;
+import UI.UIHelpers;
 
 public class ReceiptFactory {
     public Receipt createReceipt() {
+        //Receipt r = new Receipt();
+        String groceryName;
+        do{
+            groceryName = UIHelpers.promptString("Grocery name: ");
+            if(groceryName.isBlank())
+                break;
+            double groceryQuantity = UIHelpers.promptDouble("Quantity: ");
+
+            //r.addGrocery();
+        } while(true);
+
         return null;
     }
 }
