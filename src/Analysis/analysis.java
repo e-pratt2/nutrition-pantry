e23625c92b1b82d69e3590da0961d6c34e03a4c7
Anna-Mclenko <amikhailenko@ewu.edu>
2022-05-13 10:26:06 -0700

    analysis class

---
+++ src/Analysis/analysis.java
@@ -0,0 +1,23 @@
+package Analysis;
+
+import Database.Nutrition;
+import Filters.Filter;
+
+public class analysis {
+
+    public double AvgPrice(Filter filter){
+        return 0.0;
+    }
+
+    public Nutrition AvgNutrition(Filter filter){
+        return null;
+    }
+
+    public double TotalPrice(Filter filter){
+        return 0.0;
+    }
+
+    public Nutrition totalNutrition(Filter filter){
+        return null;
+    }
+}
