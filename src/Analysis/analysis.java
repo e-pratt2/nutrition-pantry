2c3ce807e66bcfdab48898638d9098b075887c65
Anna-Mclenko <amikhailenko@ewu.edu>
2022-05-13 10:44:20 -0700

    added helper methods into the analysis methods

--- src/Analysis/analysis.java
+++ src/Analysis/analysis.java
@@ -9,7 +9,7 @@ import Filters.Filter;
 public class analysis {
 
     public double AvgPrice(Filter<Store> filter, Filter<Receipt> filter1, Filter<Grocery> filter2){
-        return getAveragePrice(filter,filter1,filter2);
+        return getAveragePrice(filter, filter1, filter2);
     }
 
     public Nutrition AvgNutrition(Filter<Store> filter, Filter<Receipt> filter1, Filter<Grocery> filter2){
@@ -17,10 +17,10 @@ public class analysis {
     }
 
     public double TotalPrice(Filter<Store> filter, Filter<Receipt> filter1, Filter<Grocery> filter2){
-        return getTotalPrice(filter,filter1,filter2);
+        return getTotalPrice(filter, filter1, filter2);
     }
 
     public Nutrition totalNutrition(Filter<Store> filter, Filter<Receipt> filter1, Filter<Grocery> filter2){
-        return getTotalNutrition(filter,filter1,filter2);
+        return getTotalNutrition(filter, filter1, filter2);
     }
 }
