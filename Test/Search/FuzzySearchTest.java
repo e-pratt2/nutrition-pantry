d773e901b92518bf4f4c0d627611b40f35abb3b8
e-pratt2 <epratt2@ewu.edu>
2022-05-24 10:05:47 -0700

    test folder

---
+++ Test/Search/FuzzySearchTest.java
@@ -0,0 +1,18 @@
+package Search;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FuzzySearchTest {
+
+    @org.junit.jupiter.api.Test
+    void scoreOf() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void search() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findBestMatch() {
+    }
+}
