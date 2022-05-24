30d55ac175fdb56e47bd2aff45a38224571e99e0
e-pratt2 <epratt2@ewu.edu>
2022-05-24 11:01:41 -0700

    UiHelpers::promptString test

--- Test/UI/UIHelpersTest.java
+++ Test/UI/UIHelpersTest.java
@@ -9,20 +9,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UIHelpersTest {
 
+    //Create a way to emulate user input
+    void setInput(String s) {
+        InputStream testInput = new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8));
+        System.setIn(testInput);
+    }
+    //Create a way to read console output
+    ByteArrayOutputStream getOutput() {
+        try {
+            ByteArrayOutputStream outputBuffer = new ByteArrayOutputStream();
+            PrintStream printStream = new PrintStream(outputBuffer, true, StandardCharsets.UTF_8.name());
+
+            System.setOut(printStream);
+
+            return outputBuffer;
+        } catch(UnsupportedEncodingException e) {
+            throw new RuntimeException("Bad encoding in output text");
+        }
+    }
+
     @Test
-    void promptDouble() throws UnsupportedEncodingException {
+    void promptDouble() {
         InputStream in = System.in;
         PrintStream out = System.out;
 
         //Create an output stream to ensure printed text is correct
-        ByteArrayOutputStream outputBuffer = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(outputBuffer, true, StandardCharsets.UTF_8.name());
 
-        System.setOut(printStream);
+        ByteArrayOutputStream outputBuffer = getOutput();
 
-        //Create a way to emulate user input
-        InputStream testInput = new ByteArrayInputStream("12.34\n".getBytes(StandardCharsets.UTF_8));
-        System.setIn(testInput);
+        setInput("12.34\n");
 
         //Test with a valid double
         double value = UIHelpers.promptDouble("Double, if you please");
@@ -30,8 +45,7 @@ class UIHelpersTest {
         assertEquals(12.34, value);
 
         //Reset user input
-        testInput = new ByteArrayInputStream("bad input\n33.33\n".getBytes(StandardCharsets.UTF_8));
-        System.setIn(testInput);
+        setInput("bad input\n33.33\n");
 
         //Test with an invalid double, followed by a retry
         value = UIHelpers.promptDouble("Another, if you please");
@@ -45,6 +59,29 @@ class UIHelpersTest {
 
     @Test
     void promptString() {
+        InputStream in = System.in;
+        PrintStream out = System.out;
+
+        ByteArrayOutputStream outputBuffer = getOutput();
+
+        setInput("zoop f4\n");
+
+        //Test with a valid double
+        String value = UIHelpers.promptString("String time");
+        assertEquals(outputBuffer.toString(), "String time\r\n");
+        assertEquals("zoop f4", value);
+
+        //Reset user input
+        setInput("weird string \uD83D\uDC11\n");
+
+        //Test with an invalid double, followed by a retry
+        value = UIHelpers.promptString("Another, if you please");
+        assertEquals(outputBuffer.toString(), "String time\r\nAnother, if you please\r\n");
+        assertEquals("weird string \uD83D\uDC11", value);
+
+        //Reset in and out
+        System.setIn(in);
+        System.setOut(out);
     }
 
     @Test
