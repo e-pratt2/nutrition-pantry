2ef6cbd8df8b89a59dd06ffc26e69fae4075a0c2
Anna-Mclenko <amikhailenko@ewu.edu>
2022-05-27 14:01:20 -0700

    StoreNameFilterTest will finish the last test

---
+++ Test/Filters/StoreNameFilterTest.java
@@ -0,0 +1,49 @@
+package Filters;
+
+import Database.Store;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreNameFilterTest {
+
+    @Test
+    void constructorException(){
+        assertThrows(IllegalArgumentException.class, ()->{
+           StoreNameFilter s = new StoreNameFilter(null);
+        });
+
+        assertThrows(IllegalArgumentException.class, ()->{
+            StoreNameFilter s = new StoreNameFilter("");
+        });
+
+        Filter f = new Filter();
+        assertThrows(IllegalArgumentException.class, ()->{
+            StoreNameFilter s = new StoreNameFilter(f,"");
+        });
+
+        assertThrows(IllegalArgumentException.class, ()->{
+            StoreNameFilter s = new StoreNameFilter(f,null);
+        });
+    }
+
+    @Test
+    void accepts() {
+        StoreNameFilter s = new StoreNameFilter("Safeway");
+        assertThrows(IllegalArgumentException.class, ()->{
+            s.accepts(null);
+        });
+
+        Store st = new Store("Safeway");
+        assertTrue(s.accepts(st));
+
+        st = new Store("Walmart");
+        assertFalse(s.accepts(st));
+    }
+
+    @Test
+    void testToString() {
+        Store s = new Store("Safeway");
+        StoreNameFilter st = new StoreNameFilter("Safeway");
+    }
+}
--- src/Filters/StoreNameFilter.java
+++ src/Filters/StoreNameFilter.java
@@ -6,15 +6,25 @@ public class StoreNameFilter extends Filter<Store> {
     private String name;
 
     public StoreNameFilter(String name) {
+
+        if(name == null || name.equalsIgnoreCase(""))
+            throw new IllegalArgumentException("bad param in StoreNameFilter");
+
         this.name = name;
     }
     public StoreNameFilter(Filter<Store> child, String name) {
         super(child);
+
+        if(name == null || name.equalsIgnoreCase(""))
+            throw new IllegalArgumentException("bad param in StoreNameFilter");
         this.name = name;
     }
 
     @Override
     public boolean accepts(Store store) {
+        if(store == null)
+            throw new IllegalArgumentException("bad param StoreNameFilter");
+
         return store.getName().equalsIgnoreCase(name) && super.accepts(store);
     }
 
