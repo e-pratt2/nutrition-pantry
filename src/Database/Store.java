b16511cdf18e5e8bd3f6849236d1cc7d6228c85f
Anna-Mclenko <amikhailenko@ewu.edu>
2022-06-01 09:50:26 -0700

    started the Javadoc for Store

--- src/Database/Store.java
+++ src/Database/Store.java
@@ -8,10 +8,15 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Store implements Serializable {
+
     private HashMap<Grocery, Double> groceryPrices;
     private ArrayList<Receipt> receipts;
     private String name;
 
+    /**
+     *
+     * @param name
+     */
     public Store(String name){
         if(name == null || name.equalsIgnoreCase(""))
             throw new IllegalArgumentException("bad param in Store");
@@ -20,24 +25,57 @@ public class Store implements Serializable {
         this.name = name;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getName() {
         return this.name;
     }
+
+    /**
+     *
+     * @return
+     */
     public HashMap<Grocery, Double> getGroceryPrices(){return this.groceryPrices;}
+
+    /**
+     *
+     * @return
+     */
     public ArrayList<Receipt> getR(){return this.receipts;}
 
+    /**
+     *
+     * @param grocery
+     * @return
+     */
     public double getPriceOf(Grocery grocery){
         Double price = groceryPrices.get(grocery);
         return price == null ? 0.0 : price;
     }
 
+    /**
+     *
+     * @param receipt
+     */
     public void addReceipt(Receipt receipt){
         this.receipts.add(receipt);
     }
+
+    /**
+     *
+     * @return
+     */
     public List<Receipt> getReceipts() {
         return this.receipts;
     }
 
+    /**
+     *
+     * @param obj
+     * @return
+     */
     public boolean equals(Object obj){
 
         if(!(obj instanceof Store))
