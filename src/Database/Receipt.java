9407bfaa79ee7bfd950d20d985d2adac78419ece
Anna-Mclenko <amikhailenko@ewu.edu>
2022-06-01 09:27:24 -0700

    Javadoc for Receipt

--- src/Database/Receipt.java
+++ src/Database/Receipt.java
@@ -12,6 +12,11 @@ public class Receipt implements Serializable {
     private HashMap<Grocery, Double> groceryQuantity;
     private LocalDate date;
 
+    /**
+     * constructor that creates a Receipt object initialises all the private fields
+     * @param date Local date variable
+     * @throws IllegalArgumentException if date is null
+     */
     public Receipt(LocalDate date) {
         if(date == null)
             throw new IllegalArgumentException("bad params in receipt");
@@ -19,19 +24,37 @@ public class Receipt implements Serializable {
         this.groceryQuantity = new HashMap<>();
     }
 
+    /**
+     * this method returns the quantity of the object passed in
+     * @param grocery Grocery object to find the quantity of
+     * @return returns the quantity of the object or if null returns 0
+     */
     public double getQuantityOf(Grocery grocery) {
         Double quantity = this.groceryQuantity.get(grocery);
         return quantity == null ? 0.0 : quantity;
     }
 
+    /**
+     *
+     * @return returns the date of this receipt
+     */
     public LocalDate getDate() {
         return this.date;
     }
 
+    /**
+     * adds a grocery and quantity of the grocery into the hash map
+     * @param g Grocery object added
+     * @param quantity the quantity of the grocery
+     */
     public void addGrocery(Grocery g, double quantity) {
         this.groceryQuantity.put(g, this.getQuantityOf(g) + quantity);
     }
 
+    /**
+     *
+     * @return returns the key-set of the hash map
+     */
     public Set<Grocery> getGroceries() {
         return groceryQuantity.keySet();
     }
