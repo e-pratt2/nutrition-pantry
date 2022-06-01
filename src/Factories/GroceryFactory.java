2be6d1e3b333ab9e822f7eccdf3ec6865ebb92c6
Anna-Mclenko <amikhailenko@ewu.edu>
2022-06-01 10:34:23 -0700

    Javadoc for DIYFactory

--- src/Factories/GroceryFactory.java
+++ src/Factories/GroceryFactory.java
@@ -2,6 +2,13 @@ package Factories;
 
 import Database.Grocery;
 
+/**
+ * abstract interface
+ */
 public interface GroceryFactory {
+    /**
+     * abstract method
+     * @return returns a grocery object
+     */
     public Grocery createGrocery();
 }
