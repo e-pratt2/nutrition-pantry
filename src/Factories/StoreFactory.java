2682383a76544b94d5954b65a3c9d684c9125329
Anna-Mclenko <amikhailenko@ewu.edu>
2022-06-01 10:38:28 -0700

    Javadoc for StoreFactory

--- src/Factories/StoreFactory.java
+++ src/Factories/StoreFactory.java
@@ -3,7 +3,15 @@ package Factories;
 import Database.Store;
 import UI.UIHelpers;
 
+/**
+ * store factory that creates a store object
+ */
 public class StoreFactory {
+
+    /**
+     * creates a store object
+     * @return the store object created
+     */
     public Store createStore() {
         String str = UIHelpers.promptString("Input Store Name: ");
         return new Store(str);
