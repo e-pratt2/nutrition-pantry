6950653ef9097e2667e317b01755dfa798fcd142
e-pratt2 <epratt2@ewu.edu>
2022-06-07 11:06:49 -0700

    Javadoc in search results, comments in search

--- src/Search/FuzzySearch.java
+++ src/Search/FuzzySearch.java
@@ -17,22 +17,31 @@ public class FuzzySearch {
      * @return the strength of the match, from 0.0 to 1.0
      */
     public static float scoreOf(String query, String toTest) {
-        String[] tokens = query.toLowerCase(Locale.US).split(" ");
+        //Lowercase-ify both strings, split the query into tokens.
+        String[] tokens = query.toLowerCase(Locale.US).split(" +");
         toTest = toTest.toLowerCase(Locale.US);
 
+        //Keep track of the ngrams that match
         int totalPermutations = 0;
         int totalPassedPermutations = 0;
 
+        //For each word in the query...
         for(String token : tokens) {
+            //Permute the word with ngram length 2
             NGramPermuter sp = new NGramPermuter(token, 2);
 
+            //Increment the total number of permutations according to the permuter
             totalPermutations += sp.getPermutationCount();
+
+            //Permute!
             for(String substring : sp) {
+                //If the ngram is contained in the test string, increase the score
                 if(toTest.contains(substring))
                     totalPassedPermutations++;
             }
         }
 
+        //Score is the ratio of found ngrams/(found ngrams + unfound ngrams)
         return totalPassedPermutations/(float)totalPermutations;
     }
 
@@ -45,14 +54,20 @@ public class FuzzySearch {
      * @return a SearchResults object containing the best matches and their scores.
      */
     public static <E> SearchResults<E> search(String query, Iterable<E> items, Function<E, String> stringGetter) {
+        //Create a new results object, with the defined stringify function
         SearchResults<E> results = new SearchResults<>(stringGetter);
+
+        //For each item in the collection,
         for(E item : items) {
+            //Calculate the score
             float itemScore = scoreOf(query, stringGetter.apply(item));
 
+            //If it matches at all, add it to the results.
             if(itemScore > 0.0)
                 results.pushResult(item, itemScore);
         }
 
+        //Return the results collection
         return results;
     }
 
@@ -65,19 +80,27 @@ public class FuzzySearch {
      * @return the best match. If there are multiple objects with the same score, returns the earliest instance. Null for empty collection.
      */
     public static <E> E findBestMatch(String query, Iterable<E> items, Function<E, String> stringGetter) {
+        //Keep track of best score and object that achieved that score
         float bestScore = -1.0f;
         E bestItem = null;
+
+        //For each item in the collection...
         for(E item : items) {
+            //Calculate the score
             float itemScore = scoreOf(query, stringGetter.apply(item));
 
+            //If it's better than the previous...
             if(itemScore > bestScore){
-                if(itemScore == 1.0) //Perfect match!
+                if(itemScore == 1.0) //Perfect match! return it immediately
                     return item;
+
+                //Update best to match
                 bestScore = itemScore;
                 bestItem = item;
             }
         }
 
+        //Return the best.
         return bestItem;
     }
 }
